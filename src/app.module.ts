3f045c2c79cc7bd5bf93386a5bb5ba9e45b033c5
muhammad allief izzulhaq <alipsaliff@gmail.com>
2026-06-30 14:51:16 +0700

    feat: add app module

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
+import { HealthModule } from './modules/health/health.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { UsersModule } from './modules/users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    HealthModule,
+  ],
+})
+export class AppModule {}
