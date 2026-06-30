094b1eba4baa9492594ffaf31c62a891037c110b
muhammad allief izzulhaq <alipsaliff@gmail.com>
2026-06-30 14:52:22 +0700

    feat: add health endpoint

---
+++ src/modules/health/health.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Health')
+@Controller('health')
+export class HealthController {
+  @Get()
+  check() {
+    return {
+      service: 'nestjs-prisma-production-starter',
+      status: 'ok',
+      uptime: process.uptime(),
+      timestamp: new Date().toISOString(),
+    };
+  }
+}
