c474de7d1f277f52157e0557e6ce97633821c270
muhammad allief izzulhaq <alipsaliff@gmail.com>
2026-06-30 14:52:28 +0700

    feat: add register dto

---
+++ src/modules/auth/dto/register.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @ApiProperty({ example: 'Allif Izz' })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({ example: 'allif@example.com' })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ example: 'password123', minLength: 6 })
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
