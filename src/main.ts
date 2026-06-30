0f7ed8e09f0b80d67fd9aeec4e04d86a8507580a
muhammad allief izzulhaq <alipsaliff@gmail.com>
2026-06-30 14:51:10 +0700

    feat: add application bootstrap

---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
+import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  app.use(helmet());
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle(configService.get<string>('APP_NAME') || 'NestJS Prisma Production Starter')
+    .setDescription('Production-ready NestJS starter API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
+
+  const port = configService.get<number>('PORT') || 3000;
+  await app.listen(port);
+}
+
+bootstrap();
